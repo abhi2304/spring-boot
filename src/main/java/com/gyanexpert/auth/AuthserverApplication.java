f6f8c16ac31b84d7a21d5f1323c41ed4ff7ce11f
Abhishek Srivastava <abhicse.ieee@gmail.com>
2018-04-21 17:20:55 +0530

    First commit

---
+++ src/main/java/com/gyanexpert/auth/AuthserverApplication.java
@@ -0,0 +1,56 @@
+package com.gyanexpert.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AuthserverApplication extends WebSecurityConfigurerAdapter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthserverApplication.class, args);
+	}
+	
+	@Bean
+	public AuthenticationManager customAuthenticationManager() throws Exception {
+	  return authenticationManager();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// TODO Auto-generated method stub
+		/*http//.antMatcher("/**")
+        .authorizeRequests()
+        .antMatchers("/", "/login**")
+        .permitAll().anyRequest().authenticated().and().formLogin().and().httpBasic();*/
+		
+		http.authorizeRequests().anyRequest().authenticated().and().formLogin().and().httpBasic();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// TODO Auto-generated method stub
+		auth.inMemoryAuthentication().withUser("user")
+        .password("password").roles("USER");
+		auth.inMemoryAuthentication().withUser("abhi")
+        .password("password").roles("USER");
+	}
+	@Bean
+    public static NoOpPasswordEncoder passwordEncoder() {
+    return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
+	
+	
+	/*@RequestMapping("/user")
+	public Principal user(Principal user) {
+		return user;
+	}*/
+	
+}
---
+++ src/main/java/com/gyanexpert/auth/UserResource.java
@@ -0,0 +1,26 @@
+package com.gyanexpert.auth;
+
+import java.security.Principal;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Configuration
+@RestController
+@EnableResourceServer
+public class UserResource extends ResourceServerConfigurerAdapter {
+	 @Override
+	    public void configure(HttpSecurity http) throws Exception {
+	        http
+	            .antMatcher("/user")
+	            .authorizeRequests().anyRequest().authenticated();
+	    }
+	@RequestMapping("/user")
+	public Principal user(Principal user) {
+		return user;
+	}
+}
